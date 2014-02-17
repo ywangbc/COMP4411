3b9ce055c7743d0a712f60766cc2ee3fcdd2b4cb
ywangbc <ywangbc@gmail.com>
2014-02-17 12:21:06 +0800

    Fifth Commit By Ryan. Circle Brush Implemented

--- CircleBrush.cpp
+++ CircleBrush.cpp
@@ -4,12 +4,15 @@
 // The implementation of Point Brush. It is a kind of ImpBrush. All your brush implementations
 // will look like the file with the different GL primitive calls.
 //
-
+#include <cmath>
 #include "impressionistDoc.h"
 #include "impressionistUI.h"
 #include "CircleBrush.h"
 
 extern float frand();
+
+const double twoPi=6.2831853;
+const int triangles=100;
 
 CircleBrush::CircleBrush( ImpressionistDoc* pDoc, char* name ) :
 	ImpBrush(pDoc,name)
@@ -40,11 +43,24 @@ void CircleBrush::BrushMove( const Point source, const Point target )
 		return;
 	}
 
-	glBegin( GL_POINTS );
+	//The center of the circle
+	double x=target.x;
+	double y=target.y;
+
+	//The radius of the circle
+	int diam;
+	glGetIntegerv(GL_POINT_SIZE ,&diam);
+	double radius=diam/2.0;
+
+	glBegin(GL_TRIANGLE_FAN);
 		SetColor( source );
-
-		glVertex2d( target.x, target.y );
-
+		glVertex2f(x, y); // center of circle
+		for(int i = 0; i <= triangles;i++) { 
+			glVertex2f(
+		        x + (radius * cos(i * twoPi / triangles)), 
+			    y + (radius * sin(i * twoPi / triangles))
+			);
+		}
 	glEnd();
 }
 
