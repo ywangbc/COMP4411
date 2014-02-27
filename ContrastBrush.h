2a0c33ee9b3261fc28cd3db4f06920a79e3e3289
CY Wong <cyw617@users.noreply.github.com>
2014-02-27 14:15:17 +0800

    Implemented [Contrast Brush]

---
+++ ContrastBrush.h
@@ -0,0 +1,24 @@
+//
+// CONTRASTBrush.h
+//
+// The header file for CONTRAST Brush. 
+//
+
+#ifndef CONTRASTBRUSH_H
+#define CONTRASTBRUSH_H
+
+#include "ImpBrush.h"
+
+class ContrastBrush : public ImpBrush
+{
+public:
+	ContrastBrush( ImpressionistDoc* pDoc = NULL, char* name = NULL );
+
+	void BrushBegin( const Point source, const Point target );
+	void BrushMove( const Point source, const Point target );
+	void BrushEnd( const Point source, const Point target );
+	char* BrushName( void );
+	void ContrastBrush::SetColor (const Point);
+};
+
+#endif
